e7c24f1cecd361b8c11f4ec9204bc9a485aeccd8
Cami Anderson <camidantas@icloud.com>
2022-03-25 22:42:19 -0400

    Add project

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,56 @@
-function nameMenuItem() {
 
+function nameMenuItem(name) {
+  makeNameDelicious = "Delicious " + name;
+  return makeNameDelicious;
 }
 
+
+function createMenuItem(itemName, itemPrice, itemType){
+      menuItem = {
+        name: itemName,
+        price: itemPrice,
+        type: itemType
+  };
+  return menuItem;
+}
+
+
+function addIngredients(ingredient, ingredients){
+  if (ingredients.includes(ingredient)){ //ingredients.includes is comparing ingredient(the parameter?) to each value of the array
+    return ingredients;
+  } else {
+    ingredients.push(ingredient);
+      return ingredients;
+  }
+}
+
+function formatPrice(initialPrice){
+  formattedPrice = "$" + initialPrice;
+  return formattedPrice;
+}
+
+
+function decreasePrice (parameter){
+  less10percent = parameter * .9
+  return less10percent;
+}
+
+
+function createRecipe (title, ingredients, menuItemType){
+  addRecipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  };
+  return addRecipe;
+
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
--- src/order.js
+++ src/order.js
@@ -1,7 +1,42 @@
 
+function takeOrder (deliveryOrder, deliveryOrders){
+  if (deliveryOrders.length >= 3){
+    return deliveryOrders;
+  } else{
+  deliveryOrders.push(deliveryOrder);
+    return deliveryOrders;
+  }
+};
+
+function refundOrder (deliveryOrderNumber, deliveryOrders){
+for (var i = 0; i < deliveryOrders.length; i++){
+    if(deliveryOrders[i].orderNumber === deliveryOrderNumber) {
+      deliveryOrders.splice(i,1)
+    }
+  };
+  return deliveryOrders;
+}
+
+function listItems (orders){
+  var items = [];
+  for (var i = 0; i < orders.length; i++) {
+    items.push(orders[i].item)
+  }
+  items = items.join(", ");
+  return items;
+};
+
+function searchOrder(orders, item) {
+  // Converts all item key value into a single array using the map function
+  items = orders.map(value => value.item)
+  // Returns true or false if the item in question exists in the items list
+  return items.includes(item)
+  //includes is a function that searches an array for a specific item.
+}
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+   takeOrder,
+   refundOrder,
+   listItems,
+  searchOrder
 }
